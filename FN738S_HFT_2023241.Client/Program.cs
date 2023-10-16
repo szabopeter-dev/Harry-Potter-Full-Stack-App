edc9798e0f847f4b2c728c4085d36e7df0c4c8bf
Szabó Péter <handballcup77@gmail.com>
2023-10-16 19:11:58 +0200

    Add project files.

---
+++ FN738S_HFT_2023241.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FN738S_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hellokanyaloka");
+        }
+    }
+}
