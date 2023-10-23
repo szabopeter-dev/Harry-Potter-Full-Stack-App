e867de0a32c6bf1093a2c027dab7f4756cb4c515
Szabó Péter <handballcup77@gmail.com>
2023-10-23 21:02:33 +0200

    Houses and house types created

---
+++ FN738S_HFT_2023241.Models/Enums/HouseType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN738S_HFT_2023241.Models.Enums
+{
+    public enum HouseType : byte
+    {
+        Gryffindor,
+        Hufflepuff,
+        Ravenclaw,
+        Slytherin
+    }
+}
---
+++ FN738S_HFT_2023241.Models/House.cs
@@ -0,0 +1,21 @@
+﻿using FN738S_HFT_2023241.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN738S_HFT_2023241.Models
+{
+    public class House
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int ID { get; set; }
+        public HouseType House_name {  get; set; }
+        public string Founder_name {  get; set; }
+        
+    }
+}
