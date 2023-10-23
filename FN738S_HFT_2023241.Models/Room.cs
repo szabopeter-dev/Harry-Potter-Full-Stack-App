0f79fa9909e0fbd991d4c8cfbb30242ae0a95d11
Szabó Péter <handballcup77@gmail.com>
2023-10-23 22:51:51 +0200

    Student and room added

---
+++ FN738S_HFT_2023241.Models/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN738S_HFT_2023241.Models
+{
+    public class Room
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Range(0, 30)]
+        public int Capacity { get; set; }
+
+        public ICollection<Student> Residents { get; set; }
+    }
+}
---
+++ FN738S_HFT_2023241.Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using FN738S_HFT_2023241.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN738S_HFT_2023241.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int HouseId { get; set; }
+
+        [StringLength(240)]
+        public string Name { get; set; }
+        public HouseType HouseType { get; set; }
+
+        public Room Room { get; set; }
+
+    }
+}
