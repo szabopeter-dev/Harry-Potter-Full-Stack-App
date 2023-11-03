e315626dca9d844857dd272b6661588ad72a2920
Szabó Péter <handballcup77@gmail.com>
2023-11-03 20:05:43 +0100

    update

--- FN738S_HFT_2023241.Client/Program.cs
+++ FN738S_HFT_2023241.Client/Program.cs
@@ -11,33 +11,33 @@ namespace FN738S_HFT_2023241.Client
         static void Main(string[] args)
         {
             //testing logics linq non crud methods in main
-            var ctx = new HarrypDbContext();
+            //var ctx = new HarrypDbContext();
 
-            var studentrepo = new StudentRepository(ctx);
-            var logic = new Studentlogic(studentrepo);
+            //var studentrepo = new StudentRepository(ctx);
+            //var logic = new Studentlogic(studentrepo);
 
-            var teacherrepo = new TeacherRepository(ctx);
-            var teacherlogic = new Teacherlogic(teacherrepo);
+            //var teacherrepo = new TeacherRepository(ctx);
+            //var teacherlogic = new Teacherlogic(teacherrepo);
 
-            var houserepo = new HouseRepository(ctx);
-            var houselogic = new Houselogic(houserepo);
+            //var houserepo = new HouseRepository(ctx);
+            //var houselogic = new Houselogic(houserepo);
 
-            var subjectrepo = new SubjectRepository(ctx);
-            var subjectlogic = new Subjectlogic(subjectrepo);
+            //var subjectrepo = new SubjectRepository(ctx);
+            //var subjectlogic = new Subjectlogic(subjectrepo);
 
-            var subject_teacherrepo = new Subject_teacherRepository(ctx);
-            var subject_teacherlogic = new Subject_teacherlogic(subject_teacherrepo);
+            //var subject_teacherrepo = new Subject_teacherRepository(ctx);
+            //var subject_teacherlogic = new Subject_teacherlogic(subject_teacherrepo);
 
     
-            var nc1 = houselogic.GetStudentFromGryffindor(Models.Enums.HouseType.Gryffindor);
+            //var nc1 = houselogic.GetStudentFromGryffindor(Models.Enums.HouseType.Gryffindor);
 
-            var nc2 = subject_teacherlogic.GetTeachersByYearTaught(2000);
+            //var nc2 = subject_teacherlogic.GetTeachersByYearTaught(2000);
 
-            var nc3 = logic.GetQuidditchPlayers();
+            //var nc3 = logic.GetQuidditchPlayers();
 
-            var nc4 = teacherlogic.GetAnimagus();
+            //var nc4 = teacherlogic.GetAnimagus();
 
-            var nc5 = subjectlogic.GetTeacherFromSubject("Defence Against the Dark Arts");
+            //var nc5 = subjectlogic.GetTeacherFromSubject("Defence Against the Dark Arts");
 
             
 
