f651fa7068646e31c53cb2e3be0cc83553d7f31b
Szabó Péter <handballcup77@gmail.com>
2024-03-13 21:22:37 +0100

    NONCRUDS added

--- FN738S_HFT_202442.JSClient/wwwroot/NONCRUD/noncrud.js
+++ FN738S_HFT_202442.JSClient/wwwroot/NONCRUD/noncrud.js
@@ -5,6 +5,7 @@ function cleardisplay() {
     document.getElementById('form_GetQuidditchPlayers').style.display = "none";
     document.getElementById('form_GetRetiredTeachersFromHouse').style.display = "none";
     document.getElementById('form_GetTeacherFromSubject').style.display = "none";
+    document.getElementById('form_GetAnimagusTeachersFromASubjects').style.display = "none";
 }
 
 async function getdata03() {
@@ -147,59 +148,93 @@ async function getdata05_final() {
 
 
 
-async function getdata03() {
+async function getdata06() {
     cleardisplay();
-    document.getElementById('form_GetQuidditchPlayers').style.display = "initial";
-    document.getElementById('result_GetQuidditchPlayers').innerHTML = "";
-    await getHouses();
-    houses.forEach(function (item) {
+    document.getElementById('form_GetTeacherFromSubject').style.display = "initial";
+    document.getElementById('result_GetTeacherFromSubject').innerHTML = "";
+    await getSubjects();
+    subjects.forEach(function (item) {
         let o = document.createElement("option");
-        o.text = item.house_name;
-        o.value = item.house_name;
-        document.getElementById('slctHouse').appendChild(o);
+        o.text = item.subject_Name;
+        o.value = item.subject_Name;
+        document.getElementById('slctsubject').appendChild(o);
         console.log(o);
     })
 }
 
 
-let houses = [];
-async function getHouses() {
-    await fetch('http://localhost:3736/House')
+let subjects = [];
+async function getSubjects() {
+    await fetch('http://localhost:3736/Subject')
         .then(x => x.json())
         .then(y => {
-            houses = y;
-            console.log(houses);
+            subjects = y;
+            console.log(subjects);
         });
 }
 
-function display03() {
+function display06() {
     cleardisplay();
-    document.getElementById('form_GetQuidditchPlayers').style.display = "initial";
-    document.getElementById('result_GetQuidditchPlayers').innerHTML = "";
+    document.getElementById('form_GetTeacherFromSubject').style.display = "initial";
+    document.getElementById('result_GetTeacherFromSubject').innerHTML = "";
     noncrudcoll.forEach(t => {
-        console.log(t.studentname);
-        document.getElementById('result_GetQuidditchPlayers').innerHTML +=
-            "<tr><td>" + t.studentname + "</td></tr>";
-        console.log(t);
+        document.getElementById('result_GetTeacherFromSubject').innerHTML +=
+            "<tr><td>" + t.teachername + "</td></tr>";
     })
 }
 
 
-async function getdata03_final() {
-    let str = document.getElementById('slctHouse').value;
-
-    //for (let i = 0; i < str.length; i++) {
-    //    if (str[i] === " ") {
-    //        str = str.substring(0, i) + "%20" + str.substring(i + 1);
-    //    }
-    //}
-    str = "http://localhost:3736/Stat/GetQuidditchPlayers/" + str;
+async function getdata06_final() {
+    let str = document.getElementById('slctsubject').value;
+    str = "http://localhost:3736/Stat/GetAnimagusTeachersFromASubjects/" + str;
 
     await fetch(str)
         .then(x => x.json())
         .then(y => {
             noncrudcoll = y;
             console.log(noncrudcoll)
-            display03();
+            display06();
+        });
+}
+
+
+
+
+
+async function getdata07() {
+    cleardisplay();
+    document.getElementById('form_GetAnimagusTeachersFromASubjects').style.display = "initial";
+    document.getElementById('result_GetAnimagusTeachersFromASubjects').innerHTML = "";
+    await getSubjects();
+    subjects.forEach(function (item) {
+        let o = document.createElement("option");
+        o.text = item.subject_Name;
+        o.value = item.subject_Name;
+        document.getElementById('slctsubject2').appendChild(o);
+        console.log(o);
+    })
+}
+
+function display07() {
+    cleardisplay();
+    document.getElementById('form_GetAnimagusTeachersFromASubjects').style.display = "initial";
+    document.getElementById('result_GetAnimagusTeachersFromASubjects').innerHTML = "";
+    noncrudcoll.forEach(t => {
+        document.getElementById('result_GetAnimagusTeachersFromASubjects').innerHTML +=
+            "<tr><td>" + t.teachername + "</td></tr>";
+    })
+}
+
+
+async function getdata07_final() {
+    let str = document.getElementById('slctsubject2').value;
+    str = "http://localhost:3736/Stat/GetAnimagusTeachersFromASubjects/" + str;
+
+    await fetch(str)
+        .then(x => x.json())
+        .then(y => {
+            noncrudcoll = y;
+            console.log(noncrudcoll)
+            display07();
         });
 }
